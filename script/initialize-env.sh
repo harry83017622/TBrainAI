0e528a23740610d5f1013212a455793780cd71e4
Pitt Wu <pitt_wu@htc.com>
2021-05-12 10:40:13 +0800

    add initialization script for cloud environment

--- script/initialize-env.sh
+++ script/initialize-env.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
 export DEBIAN_FRONTEND=noninteractive
 sudo apt update && sudo apt-get -yq upgrade
+sudo apt install tesseract-ocr-chi-tra
+
 # mkdir -p ~/workspace/info
 sudo -H pip3 install flask
 sudo -H pip3 install opencv-python-headless
 sudo -H pip3 install numpy
+sudo -H pip3 install pytesseract
 
