d7b02dfbfa64be45ec46fc28b16167039e6ae092
Pitt Wu <pitt_wu@htc.com>
2021-06-03 17:51:42 +0800

    1. add GPU into readme 2. update deployment script

--- script/initialize-env.sh
+++ script/initialize-env.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
+################################################
+# new env setup flow since 2021-06-03
+################################################
+# 1. install gpu driver
+#   a. https://www.tensorflow.org/install/gpu
+#   b. https://wiki.debian.org/NvidiaGraphicsDrivers#Debian_10_.22Buster.22-1
+
+# 2. install python and relevant packages
 export DEBIAN_FRONTEND=noninteractive
 sudo apt update && sudo apt-get -yq upgrade
+sudo apt install python3-pip
 sudo apt install tesseract-ocr-chi-tra
 
-# mkdir -p ~/workspace/info
-sudo -H pip3 install flask
-sudo -H pip3 install opencv-python-headless
-sudo -H pip3 install numpy
-sudo -H pip3 install pytesseract
-sudo -H pip3 install flask-executor
+python3 -m venv --system-site-packages /home/tbrain/venv-tbrain
+pip3 install --upgrade pip
+pip3 install numpy
+pip3 install opencv-python-headless
+pip3 install pytesseract
+pip3 install flask-executor
+pip3 install tensorflow-gpu==2.4
+
+################################################
+# deprecated env setup flow before 2021-06-03
+################################################
+# export DEBIAN_FRONTEND=noninteractive
+# sudo apt update && sudo apt-get -yq upgrade
+# sudo apt install python3-pip
+# sudo apt install tesseract-ocr-chi-tra
+
+# sudo -H pip3 install flask
+# sudo -H pip3 install numpy
+# sudo -H pip3 install opencv-python-headless
+# sudo -H pip3 install pytesseract
+# sudo -H pip3 install flask-executor
+# sudo -H pip3 install tensorflow-gpu==2.4
+
 
--- script/restart-server.sh
+++ script/restart-server.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-newBuildDir="/home/pittwu/workspace/server/new-build"
-prodBuildDir="/home/pittwu/workspace/server/prod"
+set -x
 
-if [ -d "$newBuildDir/src" ]; then
+currentDir=$(dirname "$0")
+# echo "current dir: $currentDir"
+prodBuildDir=$(dirname "$currentDir")
+# echo "prod build dir: $prodBuildDir"
+prodSrcDir="$prodBuildDir/src"
+# echo "prod src dir: $prodSrcDir"
+
+# newBuildDir="$baseDir/new-build"
+# prodBuildDir="$baseDir/prod"
+
+if [ -d "$prodSrcDir" ]; then
   # kill service
   ps aux | grep 'api.py' | grep -v 'grep' | awk '{print $2;}' | xargs sudo kill -9
 
-  # remove old files
-  rm -rf $prodBuildDir
-
-  # move new build to prod
-  mv $newBuildDir $prodBuildDir
-
   # start service
-  cd "$prodBuildDir/src"
+  source /home/tbrain/venv-tbrain/bin/activate
+  cd "$prodSrcDir"
   nohup python3 api.py > nohup.out 2>&1 &
-  echo 'complete to restart service by new build'
+  echo "complete to restart service on path $prodSrcDir"
 else
-  echo "New build directory: $newBuildDir doesn't exit. Exit withoue doing anything"; exit 1;
+  echo "New build directory: $prodSrcDir does not exit. Exit withoue doing anything"; exit 1;
 fi
 
-
+set +x
 
 
